511a66c1984e4fd513e92e838eaadf1597862e9d
masanag <masaki.nagaishi@gmail.com>
2016-05-05 18:03:09 +0900

    ios9 chap01

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,3 +3,33 @@
 import UIKit
 
 var str = "Hello, playground"
+
+for i in 1...9 {
+    var line = ""
+    for j in 1...9 {
+        let n = i * j
+        if n < 10 { line += " " }
+        line += " \(n)"
+    }
+    print(line)
+}
+
+for n in 1...13 {
+    var line = ""
+    for s in ["♠︎", "♡", "♣︎", "♦︎"] {
+        var num: String
+        if n == 1 {
+            num = "A"
+        } else if n == 11 {
+            num = "J"
+        } else if n == 12 {
+            num = "Q"
+        } else if n == 13 {
+            num = "K"
+        } else {
+            num = String(n)
+        }
+        line += "\(s)\(num)"
+    }
+    print(line)
+}
