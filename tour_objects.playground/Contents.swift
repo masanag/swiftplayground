ccf1b7972da0202d749c3ea8775d984c27300fb7
masanag <masaki.nagaishi@gmail.com>
2016-05-07 13:28:07 +0900

    done protocol

--- tour_objects.playground/Contents.swift
+++ tour_objects.playground/Contents.swift
@@ -78,3 +78,126 @@ a
 a.simpleDescription
 let protocolValue: ExampleProtocol = a
 print(protocolValue.simpleDescription)
+
+enum PrinterError: ErrorType {
+    case OutOfPaper
+    case NoToner
+    case OnFire
+}
+
+func sendToPrinter(printerName: String) throws -> String {
+    if printerName == "Never Has Toner" {
+        throw PrinterError.NoToner
+    }
+    else if printerName == "Never Has Paper" {
+        throw PrinterError.OutOfPaper
+    }
+    else if printerName == "Now Firing" {
+        throw PrinterError.OnFire
+    }
+    return "Job sent"
+}
+
+do {
+    let printerResponse = try sendToPrinter("Never Has Toner")
+    print(printerResponse)
+} catch {
+    print(error)
+}
+
+do {
+    let printerResponse = try sendToPrinter("Never Has Paper")
+} catch {
+    print(error)
+}
+
+
+do {
+    let printerResponse = try sendToPrinter("Never Has Paper")
+    print(printerResponse)
+} catch PrinterError.OnFire {
+    print("aaaa")
+} catch let printerError as PrinterError {
+    print(PrinterError)
+} catch {
+    print("bbbb")
+}
+
+let printerSuccess = try? sendToPrinter("Mergenthaler")
+let printerFailure = try? sendToPrinter("Never Has Toner")
+
+var fridgeIsOpen = false
+let fridgeContent = ["milk", "eggs", "leftovers"]
+
+func fridgeContains(itemName: String) -> Bool {
+    fridgeIsOpen = true
+    defer {
+        fridgeIsOpen = false
+        print(itemName)
+    }
+    
+    let result = fridgeContent.contains(itemName)
+    return result
+}
+
+fridgeContent.contains("milk")
+fridgeContent.contains("salmon")
+fridgeIsOpen
+fridgeContains("milk")
+fridgeIsOpen
+fridgeContains("banana")
+fridgeIsOpen
+
+for i in 0..<3 {
+    defer {
+        print("i: \(i)")
+    }
+    print("Hello, \(i)")
+}
+
+func repeatItem<Item>(item: Item, numberOfTimes: Int) -> [Item] {
+    var result = [Item]()
+    for _ in 0..<numberOfTimes {
+        result.append(item)
+    }
+    return result
+}
+
+repeatItem("knock", numberOfTimes: 4)
+repeatItem(SimpleClass.init(), numberOfTimes: 2)
+repeatItem(0.11, numberOfTimes: 2)
+repeatItem([1, 2], numberOfTimes: 2)
+repeatItem((1, 2), numberOfTimes: 2)
+repeatItem(["a": 122, "b": 123], numberOfTimes: 2)
+
+
+func generateDic<T>(item: T, numberOfTimes: Int) -> [Int: T] {
+    var result = [Int: T]()
+    for i in 0..<numberOfTimes {
+        result[i] = item
+    }
+    return result
+}
+
+print(generateDic(100, numberOfTimes: 3))
+
+enum OptionalValue<Wrapped> {
+    case None
+    case Some(Wrapped)
+}
+
+var possibleInteger: OptionalValue<Int> = .None
+possibleInteger = .Some(100)
+
+func anyCommonElements <T: SequenceType, U: SequenceType where T.Generator.Element: Equatable, T.Generator.Element == U.Generator.Element> (lhs: T, _ rhs: U) -> Bool {
+    for lhsItem in lhs {
+        for rhsItem in rhs {
+            if lhsItem == rhsItem {
+                return true
+            }
+        }
+    }
+    return false
+}
+anyCommonElements([1, 2, 3], [3])
+anyCommonElements(["a", "b"], ["b"])
